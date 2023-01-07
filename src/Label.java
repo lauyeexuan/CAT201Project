3c2192a237091ae6a467e42cb2fb9c5504a10edd
lyxuan <lauyx2002@gmail.com>
2023-01-07 17:36:01 +0800

    initial commit

--- src/Label.java
+++ src/Label.java
@@ -8,7 +8,7 @@ import javax.swing.border.Border;
 
 public class Label extends JFrame implements ActionListener{
     private static final long serialVersionUID = 1L;
-    private JLabel lblSize,lblBev,lblGlass,lblReport,lbljuice;
+    private JLabel lblSize,lblBev,lblGlass,lblReport,lbljuice,lblDelivery;
     private JComboBox<String> size;
     private JRadioButton rdJuice,rdWater,rdTea,rdCoffee;
     private ButtonGroup btnGroup;
