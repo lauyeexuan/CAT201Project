24cb674ea4b7f1a9a1ef1a05d5de149e0e2068b3
lyxuan <lauyx2002@gmail.com>
2023-01-07 16:48:11 +0800

    initial commit

--- src/Label.java
+++ src/Label.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import javax.swing.*;
 import javax.swing.border.Border;
 
+//initial commit
 public class Label extends JFrame implements ActionListener{
     private static final long serialVersionUID = 1L;
     private JLabel lblSize,lblBev,lblGlass,lblReport,lbljuice;
