3d80ff06c436fbb02cfbb6d5f395163707198d96
Peijing02 <peijing02@student.usm.my>
2023-01-18 23:47:09 +0800

    Add logo

--- src/Coffee.java
+++ src/Coffee.java
@@ -2,21 +2,32 @@ import javax.swing.JOptionPane;
 
 public class Coffee extends Beverage {
     private boolean milk;
+    private int milkInt;
 
     public Coffee(String size,int amount,Label lbl) {
-        super(size,amount);
-        if(JOptionPane.showConfirmDialog(lbl, "Would you like milk?","Milk",JOptionPane.YES_NO_OPTION)==0) milk=true;
-        else milk=false;
-        double price;
-        if(size.equals("Small")) price=3.5;
-        else if(size.equals("Medium")) price=5;
-        else price=6.5;
-        if(milk) price*=1.5;
-        setPrice(price);
+        super(size, amount);
+        try {
+            Object milkObj=JOptionPane.showConfirmDialog(lbl, "Would you like milk?", "Milk", JOptionPane.YES_NO_OPTION);
+            String milkStr = milkObj.toString();
+            milkInt = Integer.parseInt(milkStr);
+            if(milkInt==0) {
+                milk=true;
+            }
+            double price = 0.0;
+            if(milkInt!=-1){
+                if (size.equals("Small")) price = 3.5;
+                else if (size.equals("Medium")) price = 5;
+                else price = 6.5;
+            }
+            if (milk) price *= 1.5;
+            setPrice(price);
+        } catch (NullPointerException e) {
+        }
     }
 
     @Override
     public String toString() {
+        if(milkInt<0) return "0 glass(es) of coffee";
         if(milk) return super.toString()+" Coffee with milk";
         else return super.toString()+" Coffee";
     }
